8e78b6accbdc8089520d0672d8d88db50cd6c1e7
Ex-32 <77823377+Ex-32@users.noreply.github.com>
2022-06-11 08:58:20 -0500

    added improvements to test

--- src/test/simple_malloc_test.c
+++ src/test/simple_malloc_test.c
@@ -20,27 +20,27 @@ int main() {
     for (size_t i = 0; i < count; ++i) {
         size_t size1 = (rand() % max_size) + 1;
         char* ptr1 = simple_malloc(size1);
-        if (ptr1 == NULL) printf("warn: Failed to allocate %lu bytes of memory", size1);
+        if (ptr1 == NULL) printf("warn: simple_malloc() returned NULL, failed to allocate %lu bytes of memory", size1);
         else write_bytes(ptr1, size1, (char)(rand() % 255));
 
         size_t size2 = (rand() % max_size) + 1;
         char* ptr2 = simple_malloc(size2);
-        if (ptr2 == NULL) printf("warn: Failed to allocate %lu bytes of memory", size2);
+        if (ptr2 == NULL) printf("warn: simple_malloc() returned NULL, failed to allocate %lu bytes of memory", size2);
         else write_bytes(ptr2, size2, (char)(rand() % 255));
 
         size_t size3 = (rand() % max_size) + 1;
         char* ptr3 = simple_malloc(size3);
-        if (ptr3 == NULL) printf("warn: Failed to allocate %lu bytes of memory", size3);
+        if (ptr3 == NULL) printf("warn: simple_malloc() returned NULL, failed to allocate %lu bytes of memory", size3);
         else write_bytes(ptr3, size3, (char)(rand() % 255));
 
         size_t size4 = (rand() % max_size) + 1;
         char* ptr4 = simple_malloc(size4);
-        if (ptr4 == NULL) printf("warn: Failed to allocate %lu bytes of memory at", size4);
+        if (ptr4 == NULL) printf("warn: simple_malloc() returned NULL, failed to allocate %lu bytes of memory", size4);
         else write_bytes(ptr4, size4, (char)(rand() % 255));
 
         size_t size5 = (rand() % max_size) + 1;
         char* ptr5 = simple_malloc(size5);
-        if (ptr5 == NULL) printf("warn: Failed to allocate %lu bytes of memory", size5);
+        if (ptr5 == NULL) printf("warn: simple_malloc() returned NULL, failed to allocate %lu bytes of memory", size5);
         else write_bytes(ptr5, size5, (char)(rand() % 255));
 
         simple_free(ptr5);
